a8a922f00d63f80f05cf8ec0f47e64f02af4b4a7
balatharunr <r.balatharun@gmail.com>
2026-02-27 10:48:33 +0530

    complete UC9

---
+++ palindrome-checker-app/src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class UseCase9PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        boolean result = check(input, 0, input.length() - 1);
+        System.out.println("Is Palindrome? : " + result);
+        scanner.close();
+    }
+
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+        return check(s, start + 1, end - 1);
+    }
+}
