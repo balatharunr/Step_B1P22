ac5f31e6a4377b08bb7cd5931db168a95c0481c1
balatharunr <r.balatharun@gmail.com>
2026-03-28 12:55:11 +0530

    implement bubble sort

---
+++ week3&4/TransactionFeeSorting.java
@@ -0,0 +1,45 @@
+
+import java.util.ArrayList;
+
+class Transaction{
+    String id;
+    double fee;
+    String timestamp;
+    public Transaction(String id, double fee, String timestamp){
+        this.id = id;
+        this.fee = fee;
+        this.timestamp = timestamp;
+    }
+
+    
+}
+
+public class TransactionFeeSorting{
+    public static void bubbleSortByFee(ArrayList<Transaction> list){
+        boolean swapped = true;
+        while(swapped){
+            swapped = false;
+            for(int i = 0; i < list.size() - 1; i++){
+                if(list.get(i).fee > list.get(i+1).fee){
+                    Transaction temp = list.get(i);
+                    list.set(i, list.get(i+1));
+                    list.set(i+1, temp);
+                    swapped = true;
+                }
+            }
+        }
+    }
+    public static void printFee(ArrayList<Transaction> list){
+        for(Transaction t : list){
+            System.out.print(t.id + ":" + t.fee + " ");
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<Transaction> list = new ArrayList<>();
+        list.add(new Transaction("id1", 10.5, "10:00"));
+        list.add(new Transaction("id2", 25.0, "09:30"));
+        list.add(new Transaction("id3", 5.0, "10:15"));
+        bubbleSortByFee(list);
+        printFee(list);
+    }
+}
